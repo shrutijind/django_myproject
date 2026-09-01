8e637cbdeb72713a97a283ebd24712e952c81ad4
shruti jindal <shrutijindal326@gmail.com>
2026-09-01 16:22:46 +0100

    Add about_view to display About page content

--- django_myproject/about/views.py
+++ django_myproject/about/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import About
+
+def about_view(request):
+    about = About.objects.first()
+    return render(request, "about/about.html", {"about": about})
